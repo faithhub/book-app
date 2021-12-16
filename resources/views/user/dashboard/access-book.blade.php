6b35ed8f88867ea55432a787d02dd8c799f4b496
faithHub <adebayoluwadara@gmail.com>
2021-12-16 22:00:26 +0100

    latest update

--- resources/views/user/dashboard/access-book.blade.php
+++ resources/views/user/dashboard/access-book.blade.php
@@ -5,19 +5,26 @@
         color: #6c757d;
         font-weight: 600;
     }
-    .cloud-container{
-    position:absolute;
-    top:0;
-    bottom:0;
-    left:0;
-    right:0;
-    overflow:hidden;
-    z-index:-1; /* Remove this line if it's not going to be a background! */
-}
+
+    .cloud-container {
+        /* position: absolute; */
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 100vh;
+        /* overflow: hidden; */
+        /* z-index: -1; */
+        /* Remove this line if it's not going to be a background! */
+    }
 </style>
 <!-- Page Content  -->
 <div id="content-page" class="content-page">
-    <div id="adobe-dc-view" class="cloud-container"></div>
+    <div class="container-fluid">
+        <div class="row">
+            <div id="adobe-dc-view" class="cloud-container"></div>
+        </div>
+    </div>
     <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
     <script type="text/javascript">
         document.addEventListener("adobe_dc_view_sdk.ready", function() {
