1ee151c3dcc72de3faa38853cae4c92e2af7c500
faithHub <adebayoluwadara@gmail.com>
2021-12-24 04:20:32 +0100

    latest update from Vendor access book

--- resources/views/vendor/books/access-book.blade.php
+++ resources/views/vendor/books/access-book.blade.php
@@ -7,15 +7,11 @@
     }
 
     .cloud-container {
-        /* position: absolute; */
         top: 0;
         bottom: 0;
         left: 0;
         right: 0;
         height: 100vh;
-        /* overflow: hidden; */
-        /* z-index: -1; */
-        /* Remove this line if it's not going to be a background! */
     }
 </style>
 <!-- Page Content  -->
