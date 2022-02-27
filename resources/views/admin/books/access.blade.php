192700b8ef267a6691301bc586a2946618510dd1
faithHub <adebayoluwadara@gmail.com>
2022-02-27 23:35:17 +0100

    latest update

--- resources/views/admin/books/access.blade.php
+++ resources/views/admin/books/access.blade.php
@@ -1,5 +1,5 @@
-@extends('admin.layouts.app')
-@section('admin')
+@extends('user.layouts.app')
+@section('user')
 <style>
     .text-me {
         color: #6c757d;
@@ -7,11 +7,15 @@
     }
 
     .cloud-container {
+        /* position: absolute; */
         top: 0;
         bottom: 0;
         left: 0;
         right: 0;
         height: 100vh;
+        /* overflow: hidden; */
+        /* z-index: -1; */
+        /* Remove this line if it's not going to be a background! */
     }
 </style>
 <!-- Page Content  -->
@@ -26,12 +30,13 @@
             <div id="adobe-dc-view" class="cloud-container"></div>
             <script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
             <script type="text/javascript">
+                console.log("{{ $material }}")
                 document.addEventListener("adobe_dc_view_sdk.ready", function() {
                     var adobeDCView = new AdobeDC.View({
                         clientId: "{{ env('ADOBECLIENTID') }}",
                         divId: "adobe-dc-view"
                     });
-                    adobeDCView.previewFile({
+                    var previewFilePromise = adobeDCView.previewFile({
                         content: {
                             location: {
                                 url: "{{ $material }}"
@@ -44,18 +49,26 @@
                         enableSearchAPIs: true,
                         showAnnotationTools: false,
                         showDownloadPDF: false,
-                        showPrintPDF: false,
+                        showPrintPDF: false
                     });
 
                     const allowTextSelection = false;
 
                     previewFilePromise.then(adobeViewer => {
                         adobeViewer.getAPIs().then(apis => {
+                            apis.search(true)
+                                .then(searchObject => console.log(searchObject))
+                                .catch(error => console.log(error));
                             apis.enableTextSelection(allowTextSelection)
                                 .then(() => console.log("Success"))
                                 .catch(error => console.log(error));
                         });
                     });
+                    // previewFilePromise.then(adobeViewer => {
+                    //     adobeViewer.getAPIs().then(apis => {
+                    //         // All viewer APIs can be invoked here
+                    //     });
+                    // });
                 });
             </script>
             @endif
